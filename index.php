d4eac6d2ff04064e1afd47ad1a6563e859a29d12
JacopoLombardi <lombajacopo@gmail.com>
2024-04-12 14:42:20 +0200

    [add] added reactive of form & compilated ReadmeFile

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 
-<!-- PHP -->
-<?php
-
-
-
-?>
-
-
 
 <!-- HTML -->
 <!DOCTYPE html>
@@ -18,19 +10,19 @@
    <!-- BOOTSTRAP -->
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-   <title>Document</title>
+   <title>Form Page</title>
 </head>
 <body>
 
    <div class="container  mt-5">
-      <form class="w-25">
+      <form class="w-25" action="page_of_landing.php" method="POST">
          <div class="mb-3">
             <label class="form-label">Nome</label>
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" name="nome">
          </div>
          <div class="mb-4">
             <label class="form-label">Cognome</label>
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" name="cognome">
          </div>
          <button type="submit" class="btn  btn-primary  fw-semibold">Submit</button>
       </form>
